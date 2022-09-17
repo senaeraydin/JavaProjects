16d4c7dbdb8e6514383a72a58fa256a7badd21a2
senaeraydin <98656151+senaeraydin@users.noreply.github.com>
2022-09-18 00:56:11 +0300

    Add files via upload

---
+++ Variables/src/Variables.java
@@ -0,0 +1,17 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		//Java is case sensitive language.
+		
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım: "+ ogrenciSayisi);
+		System.out.println("Öğrenci sayım: "+ ogrenciSayisi);
+		System.out.println("Öğrenci sayım: "+ ogrenciSayisi);
+
+	}
+
+}
