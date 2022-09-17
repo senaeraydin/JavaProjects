b62ae63688277dd5f94276f173e30fd2af45a1b1
senaeraydin <98656151+senaeraydin@users.noreply.github.com>
2022-09-18 01:22:56 +0300

    Add files via upload

---
+++ Conditionals/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 14;
+		if(number < 20) {
+			System.out.println("The number is less than 20");
+		}else if(number == 20) {
+			System.out.println("The number is equal to 20");
+		}else {
+			System.out.println("The number is greater than 20");
+		}
+		
+
+	}
+
+}
