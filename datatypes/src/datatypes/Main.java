fd559de2c02f5fad6efe019f614f2bc81822714f
senaeraydin <98656151+senaeraydin@users.noreply.github.com>
2022-09-18 01:10:43 +0300

    Add files via upload

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double number = 12.5;
+		int number2 = 12;
+		
+		
+		char character = 'A';
+		
+		String city = "Ankara";
+		
+		boolean isItTrue = false;
+
+	}
+
+}
