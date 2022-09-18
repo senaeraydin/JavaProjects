0143cabecaebc1ec0ddf6a1decc3484f797c4f0f
Sena Eraydın <98656151+senaeraydin@users.noreply.github.com>
2022-09-18 15:05:41 +0300

    Add files via upload

---
+++ LoopDemo/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//For Loop
+		
+		for(int i=1; i<=10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("For Loop is done");
+		
+		//While loop
+		int i = 1;
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		System.out.println("While Loop is done");
+		
+		//Do-While loop
+		int j=1;
+		do {
+			System.out.println(j);
+			j+=2;
+		}while(j<10);
+		
+		System.out.println("Do-While Loop is done");
+
+	}
+
+}
