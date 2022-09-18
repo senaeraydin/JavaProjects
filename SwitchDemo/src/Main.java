d2553477396faac1ef312e231a3f09b3fe5529e6
Sena Eraydın <98656151+senaeraydin@users.noreply.github.com>
2022-09-18 14:54:27 +0300

    Add files via upload

---
+++ SwitchDemo/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		char grade = 'H';
+		
+		
+		switch(grade) {
+		   case 'A':
+			   System.out.println("Perfect : You passed");
+			   break;
+		   case 'B':
+			   System.out.println("Very good : You passed");
+			   break;
+		   case 'C': 
+			   System.out.println("Good : You passed");
+			   break;
+		   case 'D': 
+			   System.out.println("Not bad : You passed");
+			   break;
+		   case 'F': 
+			   System.out.println("You didn't pass");
+			   break;
+		   default:
+			   System.out.println("You entered invalid grade");
+		}
+
+	}
+
+}
